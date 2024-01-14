13915a8a4aebb71db5a6df6ad51779e4cbfce57a
rsammer07 <ryan.sammer077@gmail.com>
2024-01-14 12:15:53 -0800

    initialized google OAuth in indexController.js

---
+++ db/connection.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const db = mongoose.connection;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+                  
+     
+mongoose.connection.on('connected', () => {
+console.log(`Connected to database`);
+})
+                  
+                  
+mongoose.connection.on('disconnected', () => {
+console.log(`Disconnected from database`);
+})
+                  
+                  
+mongoose.connection.on('error', (error) => {
+console.log(`Error connecting to`);
+console.error(error);
+})
+                  
+module.exports = mongoose
